2aa1f75a69c8af807080f256c67dc860c2c00940
AronBodzas <aron.bodzas42@gmail.com>
2022-05-17 12:50:36 +0200

    Feat: sort by category

---
+++ src/main/webapp/static/js/sortByCategory.js
@@ -0,0 +1,25 @@
+
+function getCategory(){
+    let categories = document.getElementsByClassName("dropdown-item");
+    for (let i = 0; i < categories.length; i++) {
+        categories[i].addEventListener("click", productHandler)
+    }
+}
+function productHandler(e){
+    let categoryButton = e.currentTarget;
+    let category = categoryButton.innerText;
+    sortyByCategory(category);
+}
+
+function sortyByCategory(category){
+let productsHtml = document.getElementsByClassName("col mb-5");
+    for (let i = 0; i < productsHtml.length; i++) {
+        if(productsHtml[i].dataset.category === category || category === 'All'){
+            productsHtml[i].style.display='';
+        }else{
+            productsHtml[i].style.display='none';
+        }
+    }
+}
+
+getCategory()
