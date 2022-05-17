0063f56d11179678efce7ea7197c3fd6c277f7d8
gergelybartha <gergely.bartha95@gmail.com>
2022-05-17 11:28:38 +0200

    add item to shoppingcart

--- src/main/webapp/static/js/scripts.js
+++ src/main/webapp/static/js/scripts.js
@@ -6,9 +6,32 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-$('#playButton').click(function () {
-    $('#homeCarousel').carousel('cycle');
-});
-$('#pauseButton').click(function () {
-    $('#homeCarousel').carousel('pause');
-});
+// $('#playButton').click(function () {
+//     $('#homeCarousel').carousel('cycle');
+// });
+// $('#pauseButton').click(function () {
+//     $('#homeCarousel').carousel('pause');
+// });
+
+
+let cartList = [];
+let countCart = document.getElementById("count-cart")
+
+function addToCart(){
+    let cartButtons = document.getElementsByClassName("add-cart");
+    for (let i = 0; i < cartButtons.length; i++) {
+        let prodId = cartButtons[i].getAttribute("data-id")
+        cartButtons[i].addEventListener('click',()=>cartList.push(prodId))
+        cartButtons[i].addEventListener('click',countCartItems)
+    }
+}
+
+
+function countCartItems(){
+    countCart.innerText =''
+    countCart.innerText += " " + cartList.length
+}
+
+
+addToCart()
+
